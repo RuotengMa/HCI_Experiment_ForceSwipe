9628773229d42465078bdd4ab3d2b9257bed240f
Martinet Lee <S469LEE@uwaterloo.ca>
2018-10-23 13:52:26 -0400

    force line chart complete & toggle for debug pannel and forcebar complete

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 var Pressure = require('pressure');
 
 var $ = require("jquery");
+var Chart = require("chart.js");
+
 
 
 require("bootstrap");
 
 function debugMsg(msg){
-    if(msg.constructor == Array){
-      infoStr = "array length: " + msg.length + "<br>" +
-                "array content: " + msg.join(",");
-
-      $("#debug_text").html(infoStr);
-    }
-    else
       $("#debug_text").html(msg);
 }
 
+
 var timeUpdate = [];
+var forceTimeline = [];
+var chartTimeUpdate = [];
+var chartForceTimeline = [];
+var forceLineChart = null;
+
 
 
 function forceUpdate(force){
@@ -24,14 +25,24 @@ function forceUpdate(force){
         forceBarVal = 100 * force;
         forceBarValStr = forceBarVal + "%";
 
-        timeUpdate.push(Date.now());
+        timeUpdate.push(Date.now() - tDown);
+        forceTimeline.push(force);
 
         $("#forceBar").width(forceBarValStr);
 }
 
 
+$("#debug_toggle").on("click", function(e){
+    $("#debug_panel").toggle();
+});
+
+$("#forceBar_toggle").on("click", function(e){
+    $("#forceBar").toggle();
+});
+
 
 $("#textContent").on("mousedown", function(e){
+  debugMsg("mouse down detected");
   xDown = e.pageX;
   yDown = e.pageY;
   tDown = Date.now();
@@ -45,21 +56,75 @@ $("#textContent").on("mousedown", function(e){
   yTraverse = yUp - yDown;
   timeElapsed = tUp - tDown;
 
-  if ((Math.abs(xTraverse) > 30 || Math.abs(yTraverse) > 30 ) && (timeElapsed < 1000))  {
+  if ((Math.abs(xTraverse) > 30 || Math.abs(yTraverse) > 30 ) && (timeElapsed < 2000))  {
     console.log("swipe detected!");
 
     swipeHandler(xTraverse, yTraverse, tDown, tUp);
+
   }
+
+
+    //debugMsg(forceTimeline);
+    chartTimeUpdate = [];
+    chartForceTimeline = [];
+    chartTimeUpdate = timeUpdate;
+    chartForceTimeline = forceTimeline;
+
+
+
+    if(forceLineChart != null)
+      forceLineChart.destroy();
+
+    var ctx = document.getElementById("forceLineChart");
+
+    forceLineChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+              labels: chartTimeUpdate,
+              datasets: [{
+                label:"Force applied",
+                data:chartForceTimeline,
+                borderColor: "#3e95cd",
+                fill:false
+              }]
+            },
+            options: {  responsive:false, 
+                        animation:{duration:0},
+                        scales:{
+                          yAxes:[{
+                            ticks:{
+                              max:1,
+                              min:0,
+                              stepsize:0.05
+                            }
+                          }]
+                        }
+                      }
+    });
+
+    forceLineChart.update(0);
+
+
+  timeUpdate = [];
+  forceTimeline = [];
+
 })
-;
+
+
 
 function swipeHandler( xMove, yMove, tDown, tUp ){
     console.log("Now into swipe Handler");
 
 
-    debugMsg(timeUpdate);
-    timeUpdate = [];
+    dbgInfo =   "swipe detected<br>" + 
+                "Duration: " + timeElapsed + "<br>" + 
+                "Max of Force: " + Math.max.apply(Math, forceTimeline) + "<br>"
+                ;
 
+    debugMsg(dbgInfo);
+
+    //console.log(JSON.stringify(chartTimeUpdate));
+    //console.log(JSON.stringify(chartForceTimeline));
 }
 
 
@@ -67,19 +132,19 @@ function swipeHandler( xMove, yMove, tDown, tUp ){
 Pressure.set('#textContent', {
   start: function(force, event){
         //console.log(force);
-        forceUpdate(force);
+        //forceUpdate(force);
     },
   end: function(force, event){
         //console.log(force);
-        forceUpdate(force);
+        //forceUpdate(force);
     },
   startDeepPress: function(force, event){
         //console.log(force);
-        forceUpdate(force);
+        //forceUpdate(force);
     },
   endDeepPress: function(force, event){
         //console.log(force);
-        forceUpdate(force);
+        //forceUpdate(force);
     },
   change: function(force, event){
         //console.log(force);
