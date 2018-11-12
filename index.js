afe861b31ba09f16bce9679112ad217c80b17b8e
Martinet Lee <S469LEE@uwaterloo.ca>
2018-11-11 19:50:05 -0500

    randomized text and chapter generation

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var Pressure = require('pressure');
 
 var $ = require("jquery");
 var Chart = require("chart.js");
-
+var loremIpsum = require("lorem-ipsum");
 
 
 require("bootstrap");
@@ -41,7 +41,7 @@ $("#forceBar_toggle").on("click", function(e){
 });
 
 
-$("#textContent").on("mousedown", function(e){
+$("#touchDetectArea").on("mousedown", function(e){
   debugMsg("mouse down detected");
   xDown = e.pageX;
   yDown = e.pageY;
@@ -59,8 +59,7 @@ $("#textContent").on("mousedown", function(e){
   if ((Math.abs(xTraverse) > 30 || Math.abs(yTraverse) > 30 ) && (timeElapsed < 2000))  {
     console.log("swipe detected!");
 
-    swipeHandler(xTraverse, yTraverse, tDown, tUp);
-
+    swipeHandler(xTraverse, yTraverse);
   }
 
 
@@ -110,9 +109,52 @@ $("#textContent").on("mousedown", function(e){
 
 })
 
+var DIRECTION = Object.freeze({"LEFT":1, "RIGHT": 2, "UP":3, "DOWN":4});
+
+function getDirection(xMove, yMove){
+  if(Math.abs(xMove) >= Math.abs(yMove)){
+    if(xMove > 0)
+      return DIRECTION.RIGHT;
+    else
+      return DIRECTION.LEFT;
+  }
+  else{
+    if(yMove > 0)
+      return DIRECTION.UP;
+    else
+      return DIRECTION.DOWN;
+  }
+}
+
+function getCurrentChapter(){
+  var viewportTop = $(window).scrollTop();
+  var viewportBottom = viewportTop + $(window).height();
 
 
-function swipeHandler( xMove, yMove, tDown, tUp ){
+}
+
+
+function forceSwipeHandler(swpDir){
+  switch(swpDir){
+    case DIRECTION.LEFT:
+      console.log("Navigate to previous chapter");
+      //PrevChapter();
+      break;
+    case DIRECTION.RIGHT:
+      console.log("Navigate to next chapter");
+      //NextChapter();
+      break;
+    default:
+      console.log("...");
+  }
+}
+
+function normalSwipeHandler(swpDir){
+
+}
+
+
+function swipeHandler( xMove, yMove ){
     console.log("Now into swipe Handler");
 
 
@@ -120,6 +162,15 @@ function swipeHandler( xMove, yMove, tDown, tUp ){
                 "Duration: " + timeElapsed + "<br>" + 
                 "Max of Force: " + Math.max.apply(Math, forceTimeline) + "<br>"
                 ;
+
+    swipeDirection = getDirection(xMove, yMove);
+
+    if (Math.max.apply(Math, forceTimeline) > 0.5){ 
+        forceSwipeHandler(swipeDirection);
+    }
+    else{
+        normalSwipeHandler(swipeDirection);
+    }
 
     debugMsg(dbgInfo);
 
@@ -129,7 +180,7 @@ function swipeHandler( xMove, yMove, tDown, tUp ){
 
 
 
-Pressure.set('#textContent', {
+Pressure.set('#touchDetectArea', {
   start: function(force, event){
         //console.log(force);
         //forceUpdate(force);
@@ -153,4 +204,42 @@ Pressure.set('#textContent', {
   }
   );
 
+
+function genChapter(chapterNum){
+
+  minParagraphs = 50;
+  rangeParagraphs = 30;
+
+  randParagraphs = Math.floor(Math.random() * rangeParagraphs + minParagraphs); 
+
+  console.log(randParagraphs);
+
+  liRandomText = loremIpsum({
+      count: randParagraphs,
+      units: 'paragraphs',
+      sentenceLowerBound: 5,
+      sentenceUpperBound: 20,
+      paragraphLowerBound: 3,
+      paragraphUpperBound: 10,
+      format: 'plain'
+  });
+
+  liRandomText = "<div class='chContainer' id='ch"+chapterNum+"'><h2 class='chTitle'>Chapter " + chapterNum + "</h2>" + liRandomText + "</div>"
+
+
+  output = liRandomText;
+
+  return output;
+}
+
+
+$(document).ready(function(){
+
+    output = genChapter(1) + genChapter(2) + genChapter(3);
+
+
+
+    $("#randomTextArea").html(output);
+
+}); 
 
